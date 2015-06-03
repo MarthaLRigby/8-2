5fabe9440f640dacb586a5c6ac59a33d4aba68b3
Jake Mckenna <Jake.Mckenna@softwire.com>
2015-06-03 10:21:53 +0100

    Review comments

--- 8-2/Program.cs
+++ 8-2/Program.cs
@@ -17,12 +17,14 @@ namespace _8_2
     class Tester
     {
         public void run()
+        // Again, capitalise method names.
         {
             Dog dog = new Dog(0,"brown");
             string color = dog.Color;
             Console.WriteLine("The color of the dog is: {0}",color);
             Console.WriteLine("What is the weight?");
             int w = Convert.ToInt32(Console.ReadLine());
+            // Again, full names for variables.
             dog.Weight = w;
             Console.WriteLine("The weight of the dog is now: " + dog.Weight);
         }
@@ -33,6 +35,10 @@ namespace _8_2
         private string color;
         public int Weight { get; set; }
         public string Color
+        // Although the book does explicitly tell you to create the private variable, you can actually use 
+        // an auto-property as you did with Weight - just
+        // public string Color {get; set;}
+        // will set up a private variable and do all the getters and setters below for you.
         {
             get 
             { 
@@ -44,6 +50,7 @@ namespace _8_2
             }
         }
         public Dog(int w, string c)
+        // Again, use this.weight = weight etc
         {
             weight = w;
             color = c;
